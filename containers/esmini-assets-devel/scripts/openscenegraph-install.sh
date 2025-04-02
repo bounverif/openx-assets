3c325d65b11cb8327256ab8c0bd05dc4ad64f35c
Dogan Ulus <dogan.ulus@bogazici.edu.tr>
2025-04-02 12:24:16 +0000

    Update script

--- containers/esmini-assets-devel/scripts/openscenegraph-install.sh
+++ containers/esmini-assets-devel/scripts/openscenegraph-install.sh
@@ -3,6 +3,7 @@ set -e
 
 # Set OpenSceneGraph version
 OPENSCENEGRAPH_VERSION="${OPENSCENEGRAPH_VERSION:-3.6.5}"
+OPENSCENEGRAPH_INSTALL_PREFIX="${OPENSCENEGRAPH_INSTALL_PREFIX:-/usr/local}"
 FBXSDK_INSTALL_PREFIX="${FBXSDK_INSTALL_PREFIX:-/usr/local}"
 
 # Clone OpenSceneGraph repository
@@ -18,6 +19,9 @@ cmake . \
   -DFBX_XML2_LIBRARY_DEBUG="xml2" \
   -DFBX_ZLIB_LIBRARY="z" \
   -DFBX_ZLIB_LIBRARY_DEBUG="z" \
-  -DCMAKE_INSTALL_PREFIX="/usr/local"
+  -DCMAKE_INSTALL_PREFIX="${OPENSCENEGRAPH_INSTALL_PREFIX}"
 
 cmake --build . --config Release --target install
+
+# Clean up
+rm -rf /tmp/osg
