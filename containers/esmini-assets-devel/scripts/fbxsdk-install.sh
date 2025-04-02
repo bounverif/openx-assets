c67c1cff5f8a5f012a604ace8e5c2ff1bc8729a8
Dogan Ulus <dogan.ulus@bogazici.edu.tr>
2025-04-02 15:26:32 +0000

    Update devel container

--- containers/esmini-assets-devel/scripts/fbxsdk-install.sh
+++ containers/esmini-assets-devel/scripts/fbxsdk-install.sh
@@ -8,13 +8,14 @@ FBXSDK_VERSION_PATCH="${FBXSDK_VERSION_PATCH:-7}"
 FBXSDK_VERSION="${FBXSDK_VERSION_MAJOR}${FBXSDK_VERSION_MINOR}${FBXSDK_VERSION_PATCH}"
 FBXSDK_VERSION_DASHED="${FBXSDK_VERSION_MAJOR}-${FBXSDK_VERSION_MINOR}-${FBXSDK_VERSION_PATCH}"
 FBXSDK_DOWNLOAD_URL="https://damassets.autodesk.net/content/dam/autodesk/www/files/fbx${FBXSDK_VERSION}_fbxsdk_gcc_linux.tar.gz"
-FBXSDK_INSTALL_PREFIX="${FBXSDK_INSTALL_PREFIX:-/usr/local}"
+FBXSDK_INSTALL_PREFIX="${FBXSDK_INSTALL_PREFIX:-/opt/fbxsdk}"
 
 # Create necessary directories
 mkdir -p /tmp/fbxsdk
 mkdir -p ${FBXSDK_INSTALL_PREFIX}
 
 # Download and extract FBX SDK
+echo "Downloading $FBXSDK_DOWNLOAD_URL"
 curl -kL --user-agent "Mozilla/5.0" "$FBXSDK_DOWNLOAD_URL" | tar xzv -C /tmp/fbxsdk
 
 # Install FBX SDK
