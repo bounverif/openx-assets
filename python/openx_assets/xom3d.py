c579c88d733a492c600ea6811f0caef4825aa89a
Dogan Ulus <dogan.ulus@bogazici.edu.tr>
2025-06-27 09:27:09 +0300

    Add a default xoma file template

--- python/openx_assets/xom3d.py
+++ python/openx_assets/xom3d.py
@@ -8,3 +8,52 @@ class Context:
     asset: Optional[Dict] = None
     material_schema: Optional[Dict] = None
     materials: Optional[Dict] = None
+
+
+@dataclass
+class FileTemplates:
+    xoma_1_0_0 = {
+        "metadata": {
+            "name": "",
+            "description": "",
+            "uuid": "",
+            "authors": [],
+            "license": "",
+            "copyrights": [],
+            "openMaterial3dVersion": "1.0.0",
+            "assetVersion": "",
+            "assetType": "",
+            "objectClass": "vehicle",
+            "boundingBox": {"x": [0.0, 0.0], "y": [0.0, 0.0], "z": [0.0, 0.0]},
+            "vehicleClassData": {
+                "vehicleCategory": "car",
+                "axles": {
+                    "frontAxle": {
+                        "maxSteering": 0.0,
+                        "wheelDiameter": 0.0,
+                        "trackWidth": 0.0,
+                        "positionX": 0.0,
+                        "positionZ": 0.0,
+                    },
+                    "rearAxle": {
+                        "maxSteering": 0.0,
+                        "wheelDiameter": 0.0,
+                        "trackWidth": 0.0,
+                        "positionX": 0.0,
+                        "positionZ": 0.0,
+                    },
+                },
+                "performance": {
+                    "maxSpeed": 0.0,
+                    "maxAcceleration": 0.0,
+                    "maxDeceleration": 0.0,
+                },
+            },
+            "meshCount": 1,
+            "triangleCount": 1,
+            "animated": False,
+            "textureResolutions": [],
+            "pbrMaterialWorkflow": "metallic",
+            "normalMapFormat": "OpenGL",
+        }
+    }
